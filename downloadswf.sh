280ff3af893cf3f785e090023ef9885f723da090
Jianqing Peng <pengjianqing@gmail.com>
2014-07-12 22:46:52 +0800

    support download jpg and swf to different directory.

--- downloadswf.sh
+++ downloadswf.sh
@@ -5,15 +5,34 @@ echo "./$@ downloadurls.txt"
 echo "Get the cookie, save to ./cookie"
 wget -q --save-cookies=cookie --keep-session-cookies http://joy4girl.com/ >/dev/null
 rm index.html*
-OUTPUT=./output
+OUTPUT_SWF=./output_swf
+OUTPUT_JPG=./output_jpg
+OUTPUT_OTHER=./output_other
 
-mkdir -p ${OUTPUT}
+mkdir -p ${OUTPUT_SWF}
+mkdir -p ${OUTPUT_JPG}
+mkdir -p  ${OUTPUT_OTHER}
 
 URLS=$1
 echo "Start to download files from ${URLS}"
 while read line
 do
     echo "Downloading ${line}"
-    wget -q --load-cookies=cookie --keep-session-cookies ${line} >/dev/null
-    mv *.swf* ${OUTPUT}
+
+    if [[ ${line} == *.jpg ]];then
+        echo "Downloading jpg to ${OUTPUT_JPG} ..."
+        cd ${OUTPUT_JPG}
+        wget -q --load-cookies=../cookie --keep-session-cookies ${line} >/dev/null
+        cd ..
+    elif [[ ${line} == *.swf ]];then
+        echo "Downloading swf to ${OUTPUT_SWF} ..."
+        cd ${OUTPUT_SWF}
+        wget  --load-cookies=../cookie --keep-session-cookies ${line}
+        cd ..
+    else
+        echo "Downloading other to ${OUTPUT_OTHER} ..."
+        cd ${OUTPUT_OTHER}
+        wget -q --load-cookies=../cookie --keep-session-cookies ${line} >/dev/null
+        cd ..
+    fi
 done<${URLS}
